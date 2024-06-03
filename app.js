04aaa35ac179159eacc56adb0fda0e9e085fdf63
Udayd20 <udaydevraj2002@gmail.com>
2024-06-03 18:01:24 -0400

    Added files

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const express = require('express');
 const expresslayout=require('express-ejs-layouts')
 const app = express();//Creats express application and it needs port also
-const PORT = 5000 || process.env.PORT;//process.env.PORT is used to host on a server
+const PORT = 6000 || process.env.PORT;//process.env.PORT is used to host on a server
 
 
 app.use(express.static('public'));
--- server/routes/main.js
+++ server/routes/main.js
@@ -7,8 +7,18 @@ const router = express.Router()
 //Routes
 router.get('',(req,res)=>
     {
-        res.send("Hello World");
+        // res.send("Hello World");
+        res.render('index');
     })
+router.get('/about',(req,res)=>
+        {
+            res.render('index');
+            // res.send("Hello World");
+          
+        })   
 
-
+router.get('/login',(req,res)=>
+{
+    res.render('login');
+})
 module.exports = router;
