38da54738189728d366c29c0d8517e60267af716
Udayd20 <udaydevraj2002@gmail.com>
2024-06-24 17:12:41 -0400

    First commit

--- app.js
+++ app.js
@@ -1,21 +1,53 @@
-//Creating express server
-
 require('dotenv').config();
 
 const express = require('express');
-const expresslayout=require('express-ejs-layouts')
-const app = express();//Creats express application and it needs port also
-const PORT = 6000 || process.env.PORT;//process.env.PORT is used to host on a server
+const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
+const connectDB = require('./server/config/db');
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to DB
+connectDB();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI
+  }),
+}));
 
 app.use(express.static('public'));
-//Templating engine
-app.use(expresslayout);
-app.set('layout','./layouts/main');
-app.set('view engine','ejs');
 
-app.use('/',require('./server/routes/main.js'))
-app.listen(PORT,()=>
-{
-    console.log(`App is running on port ${PORT}`);
-})
+// Middleware to set currentRoute
+app.use((req, res, next) => {
+  res.locals.currentRoute = req.path;
+  next();
+});
+
+// Templating Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.locals.isActiveRoute = isActiveRoute;
+
+app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,24 +1,92 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
 
-const express =require ('express')
+// Routes
+router.get('', async (req, res) => {
+    try {
+        const locals = {
+            title: "NodeJs Blog",
+            description: "Simple blog created with Nodejs, express and mongodb"
+        };
+        let perPage = 25;
+        let page = req.query.page || 1;
 
-const router = express.Router()
+        const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+            .exec();
 
+        const count = await Post.countDocuments();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
 
-//Routes
-router.get('',(req,res)=>
-    {
-        // res.send("Hello World");
-        res.render('index');
-    })
-router.get('/about',(req,res)=>
-        {
-            res.render('index');
-            // res.send("Hello World");
-          
-        })   
+        res.render('index', {
+            locals,
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null,
+            currentRoute:`/`
+        });
+    } catch (error) {
+        console.log(error);
+    }
+    // res.send("Hello World");
+});
 
-router.get('/login',(req,res)=>
-{
+router.get('/post/:id', async (req, res) => {
+    try {
+        let slug = req.params.id;
+
+        const data = await Post.findById({ _id: slug });
+
+        const locals = {
+            title: data.title,
+            description: "Simple Blog Created with NodeJs, Express and MongoDb"
+           
+        };
+        res.render('post', { locals, data, currentRoute:`/post/${slug}` });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.post('/search', async (req, res) => {
+    try {
+        const locals = {
+            title: "Search",
+            description: "Simple blog created with NodeJS, Express and MongoDb"
+        };
+        let searchTerm = req.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9]/g, "");
+
+        const data = await Post.find({
+            $or: [
+                { title: { $regex: new RegExp(searchNoSpecialChar, 'i') } },
+                { body: { $regex: new RegExp(searchNoSpecialChar, 'i') } }
+            ]
+        });
+        res.render("search", {
+            data,
+            locals
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.get('/about', (req, res) => {
+    res.render('about',{ currentRoute:`/about`});
+    // res.send("Hello World");
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact',{ currentRoute:`/contact`});
+    
+});
+
+router.get('/login', (req, res) => {
     res.render('login');
-})
+});
+
 module.exports = router;
